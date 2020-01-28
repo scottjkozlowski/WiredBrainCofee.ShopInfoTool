d6ea4056a8eeb6a5eedd5229bdd28a8fc9e2ca20
Scott Kozlowski <scott.kozlowski@odh.ohio.gov>
2020-01-28 11:37:24 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCofee.ShopInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
